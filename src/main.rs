f38dbb0c37de4a4f49eb71c198f04e54b997706a
jrshi <jrs9538@g.rit.edu>
2026-01-12 15:05:20 -0500

    Implemented collection of args

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let file = File::open(args[1].clone());
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => {
+            match error.kind() {
+                std::io::ErrorKind::NotFound => {
+                    panic!("File not found: {}", error)
+                }
+                _ => {
+                    panic!("Error opening file: {}", error)
+                }
+            }
+        }
+    };
+    
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        match line {
+            Ok(line) => println!("{}", line),
+            Err(error) => {
+                panic!("Error reading line: {}", error)
+            }
+        }
+    }
+}
